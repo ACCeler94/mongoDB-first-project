b72af71e5561cf134a31ff862f71687a7085ea4d
Artur C <araghast94@icloud.com>
2023-10-15 13:37:40 +0200

    Modify endpoints to use mongoDB

---
+++ routes/products.routes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
+
+router.get('/products', (req, res) => {
+  req.db.collection('products').find().toArray().then(data => {
+    res.json(data);
+  }).catch(err => {
+    res.status(500).json({ message: err });
+  })
+});
+
+router.get('/products/random', (req, res) => {
+  req.db.collection('products').aggregate([{ $sample: { size: 1 } }]).toArray().then(
+    data => res.json(data)
+  ).catch(err => {
+    res.status(500).json({ message: err });
+  })
+});
+
+router.get('/products/:id', (req, res) => {
+  req.db.collection('products').findOne({ _id: ObjectId(req.params.id) }).then(data => {
+    if (!data) {
+      res.status(404).json({ message: 'Not found' })
+    }
+  }).catch(err => {
+    res.status(500).json({ message: err });
+  })
+});
+
+router.post('/products', (req, res) => {
+  const { name, client } = req.body;
+  req.db.collection('products').insertOne({ name, client }).then(() => {
+    res.json({ message: 'OK' });
+  }).catch(err => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.put('/products/:id', (req, res) => {
+  const { name, client } = req.body;
+  req.db.collection('products').updateOne({ name, client, _id: ObjectId(req.params.id) }).then(() => res.json({ message: 'OK' }
+  )).catch(err => {
+    res.status(500).json({ message: err })
+  })
+});
+
+router.delete('/products/:id', (req, res) => {
+  req.db.collection('products').deleteOne({ _id: ObjectId(req.params.id) }).then(() => {
+    res.json({ message: 'OK' });
+  }).catch(err => {
+    res.status(500).json({ message: err })
+  })
+});
+
+module.exports = router;
