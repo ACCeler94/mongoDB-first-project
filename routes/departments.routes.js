d4b9278d83d58d25da9f6b741a5358a4673c1e9d
Artur C <araghast94@icloud.com>
2023-10-16 20:19:55 +0200

    Add modified document to json response in departments

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -45,7 +45,7 @@ router.post('/departments', async (req, res) => {
     const { name } = req.body;
     const newDepartment = new Department({ name: name });
     await newDepartment.save();
-    res.json({ message: 'OK' });
+    res.json({ message: 'OK', document: newDepartment });
 
   } catch (err) {
     res.status(500).json({ message: err });
@@ -59,7 +59,7 @@ router.put('/departments/:id', async (req, res) => {
     if (dep) {
       dep.name = name;
       await dep.save();
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', document: dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -73,7 +73,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', document: dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
